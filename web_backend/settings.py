feb4798dd7eb52eba31c519965b1d219da87d3da
shalchian <shalchianmh@gmail.com>
2023-11-23 23:06:26 +0330

    adding apps

--- web_backend/settings.py
+++ web_backend/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    "rest_framework.authtoken"
+    "rest_framework.authtoken",
+    'user',
+    'shop',
+    'manager',
+    'cart',
+
 ]
 
 MIDDLEWARE = [
